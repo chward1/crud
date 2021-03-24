7098931335b8db3c66ff8c23a683bc461e14164b
chward1 <christina.harney@gmail.com>
2021-03-23 21:09:19 -0700

    added cafeProject and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './carousel.css'
-import 'popper.js/dist/popper.min.js'
-import portrait from './portrait.jpg'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import CafeProject from './components/CafeProject';
 
-function App() {
-
+const App = () => {
   const handleOpenCloseNav = () => {
     const navBarToggler = document.getElementsByClassName("navbar-toggler");
     if (navBarToggler[0].classList.contains("collapsed")) {
@@ -34,106 +37,55 @@ function App() {
 
 
   return (
-    <div>
-      <header>
-        <nav id="nav" className="navbar navbar-expand-md navbar-dark fixed-top">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#"></a>
-            <button onClick={() => handleOpenCloseNav()} className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#aboutMe">About Me</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://www.linkedin.com/in/christina-ward" tabIndex="-1">LinkedIn</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://github.com/chward1/crud">GitHub</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#contact">Contact</a>
-                </li>
-              </ul>
+    < Router >
+      <div>
+        <header>
+
+          <nav id="nav" className="navbar navbar-expand-md navbar-light fixed-top">
+            <div className="container-fluid">
+              <a className="navbar-brand d-block d-sm-none" id="brand" href="#">Christina Ward</a>
+              <button onClick={() => handleOpenCloseNav()} className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+
+              <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#aboutMe">About Me</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.linkedin.com/in/christina-ward" tabIndex="-1">LinkedIn</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://github.com/chward1/crud">GitHub</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#contact">Contact</a>
+                  </li>
+                </ul>
+                <div className="d-none d-md-block d-lg-block d-xl-block">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a className="navbar-brand" href="#">Christina Ward
+          </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </nav>
-      </header>
+          </nav>
 
-      <main>
-        <div className="row featurette first justify-content-evenly">
-          <div className="col-xs-12 col-md-4">
-            <h2 className="featurette-heading">Hi! I'm Christina.</h2>
-            <p className="lead">I develop beautiful and functional websites using HTML5, CSS3, JavaScript, Bootstrap5. Here are some examples of my work below.</p>
-          </div>
-          <div className="col-xs-12 col-md-5">
-            <img className="rounded-circle img-fluid" src={portrait} alt="Christina Ward" />
-
-
-          </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-        <div class="container marketing">
-          <div class="row">
-            <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            </div>
-            <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-              <h2>Heading</h2>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            </div>
-            <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            </div>
-          </div>
-        </div>
-
-        <hr className="featurette-divider"></hr>
-
-        <div className="row featurette" id="aboutMe">
-          <div className="col-md-7 order-md-2">
-            <h2 className="featurette-heading">A little more about me...</h2>
-            <p className="lead">Originally from southern California and now based out of Sebastopol, CA.  I am truly grateful for my family, my dogs (Lucy and newest addition Penny - featured left), and any time to relax outdoors.</p>
-            <p className="lead"></p>
-          </div>
-          <div className="col-md-5 order-md-1">
-            <img className="rounded-circle featurette-image img-fluid mx-auto" width="500" height="500" src="penny.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" />
-
-          </div>
-        </div>
-
-        <hr className="featurette-divider" />
-        <div className="row featurette" id="aboutMe">
-          <div className="col-md-7 order-md-2">
-            <h2 className="featurette-heading">Contact Information</h2>
-            <p className="lead">I would to hear from you! Feel free to reach out.</p>
-          </div>
-          <div className="col-md-5 order-md-1">
-            <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /></svg>
-
-          </div>
-        </div>
-
-        <hr className="featurette-divider" />
-        <footer className="container">
-          <p>Christina.harney@gmail.com</p>
-          <p className="float-end"><a href="#">Back to top</a></p>
-
-        </footer>
-      </main>
-    </div >
-
-  );
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/cafeproject" component={CafeProject} />
+          </Switch>
+        </main>
+      </div>
+    </Router >
+  )
 }
 
 export default App;
---
+++ src/components/CafeProject.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import '../styles/cafeProjectStyles/style.css';
+
+const CafeProject = () => {
+    return (
+        <React.Fragment>
+            <header>
+                <img className="logo" src="https://content.codecademy.com/courses/freelance-1/unit-4/img-tea-cozy-logo.png" alt=" Tea Cozy Logo" />
+                <nav>
+                    <ul className="navigation">
+                        <li><a href="#mission-target">Mission</a></li>
+                        <li><a href="#featured-tea-target">Featured Tea</a></li>
+                        <li><a href="#locations-target">Locations</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <a href="#mission-target" id="mission-target"></a>
+            <section className="mission-container">
+                <div className="mission-statement-container">
+                    <h2>Our Mission</h2>
+                    <h4>Handpicked, Artisanally Curated, Free Range, Sustainable, Small Batch, Fair Trade, Organic Tea</h4>
+                </div>
+            </section>
+
+            <a href="" id="featured-tea-target"></a>
+            <section className="featured-container">
+                <div className="featured-statement">
+                    <h2>Tea of the Month</h2>
+                    <h4>What's Steeping at The Tea Cozy?</h4>
+                </div>
+                <div className="flexbox-center">
+                    <div className="img-container">
+                        <figure>
+                            <img src="https://content.codecademy.com/courses/freelance-1/unit-4/img-berryblitz.jpg" alt="Fall Berry Blitz Tea" />
+                            <figcaption>Fall Berry Blitz Tea</figcaption>
+                        </figure>
+                    </div>
+                    <div className="img-container">
+                        <figure>
+                            <img src="https://content.codecademy.com/courses/freelance-1/unit-4/img-spiced-rum.jpg" alt="Spiced Rum Tea" />
+                            <figcaption>Spiced Rum Tea</figcaption>
+                        </figure>
+                    </div>
+                    <div className="img-container">
+                        <figure>
+                            <img src="https://content.codecademy.com/courses/freelance-1/unit-4/img-donut.jpg" alt="Seasonal Donuts" />
+                            <figcaption>Seasonal Donuts</figcaption>
+                        </figure>
+                    </div>
+                    <div className="img-container">
+                        <figure>
+                            <img src="https://content.codecademy.com/courses/freelance-1/unit-4/img-myrtle-ave.jpg" alt="Myrtle Ave Tea" />
+                            <figcaption>Myrtle Ave Tea</figcaption>
+                        </figure>
+                    </div>
+                    <div className="img-container">
+                        <figure>
+                            <img src="https://content.codecademy.com/courses/freelance-1/unit-4/img-bedford-bizarre.jpg" alt="Bedford Bizarre Tea" />
+                            <figcaption>Bedford Bizarre Tea</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </section>
+
+            <a href="" id="locations-target"></a>
+            <section className="locations-container">
+                <div className="locations-statement">
+                    <h2>Locations</h2>
+                </div>
+                <div className="address-container">
+                    <h3>Downtown</h3>
+                    <h4>384 West 4th St</h4>
+                    <h4>Suite 108</h4>
+                    <h4>Portland, Maine</h4>
+                </div>
+                <div className="address-container">
+                    <h3>East Bayside</h3>
+                    <h4>3433 Phisherman's Avenue</h4>
+                    <h4>(Northwest Corner)</h4>
+                    <h4>Portland, Maine</h4>
+                </div>
+                <div className="address-container">
+                    <h3>Oakdale</h3>
+                    <h4>515 Crescent Avenue</h4>
+                    <h4>Second Floor</h4>
+                    <h4>Portland, Maine</h4>
+                </div>
+            </section>
+
+            <section className="contact-container">
+                <h2>The Tea Cozy</h2>
+                <h5>contact@theteacozy.com</h5>
+                <h4>917-55-8904</h4>
+            </section>
+
+            <footer>
+                <h5>copyright The Tea Cozy 2017</h5>
+            </footer>
+        </ React.Fragment>
+
+    )
+};
+
+export default CafeProject;
